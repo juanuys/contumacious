6e773b1d2747af004ef5c06ba6cc14a3c6199160
Juan M Uys <juan+github.com@uys.io>
2021-05-02 09:32:55 +0100

    backgrounds

--- src/battle/Arena.gd
+++ src/battle/Arena.gd
@@ -4,5 +4,12 @@
 
 extends Node2D
 
+onready var hills = $Train/Hills
+onready var trees = $Train/Trees
+
+export(float) var hills_scroll_speed = 0.01
+export(float) var trees_scroll_speed = 0.07
+
 func _ready():
-	pass
+	hills.material.set_shader_param("scroll_speed", hills_scroll_speed)
+	trees.material.set_shader_param("scroll_speed", trees_scroll_speed)
