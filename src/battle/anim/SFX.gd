69e8371162d94b7cde44f6fae745850e622fc68d
Juan M Uys <juan+github.com@uys.io>
2021-05-08 11:00:13 +0100

    random attack sounds

---
+++ src/battle/anim/SFX.gd
@@ -0,0 +1,21 @@
+extends AudioStreamPlayer
+
+var assets = []
+func _ready():
+	var path = "res://assets/audio/sfx/attacks"
+	var dir = Directory.new()
+	dir.open(path)
+	dir.list_dir_begin()
+	while true:
+		var file_name = dir.get_next()
+		if file_name == "":
+			#break the while loop when get_next() returns ""
+			break
+		elif !file_name.begins_with(".") and !file_name.ends_with(".import"):
+			assets.append(load(path + "/" + file_name))
+	dir.list_dir_end()
+
+func play_sound():
+	var asset = assets[randi() % assets.size()]
+	self.stream.audio_stream = asset
+	self.play()
