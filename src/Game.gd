3c187e3437d4fd9b8976d80f2358d4456f24d86d
Juan M Uys <juan+github.com@uys.io>
2021-05-11 10:01:20 +0100

    turn down music/ambient volume

--- src/Game.gd
+++ src/Game.gd
@@ -10,7 +10,7 @@ func _ready():
 	var stream = load("res://assets/audio/music/CC3.ogg")
 	music.set_stream(stream)
 	music.pitch_scale = 1
-	fade_in.interpolate_property(music, "volume_db", -80, -5, 2.0, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
+	fade_in.interpolate_property(music, "volume_db", -80, -10, 2.0, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
 	fade_in.start()
 	yield(get_tree().create_timer(0.1), "timeout")
 	music.play()
